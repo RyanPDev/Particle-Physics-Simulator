fd63dcccc7bfd1b39dfb5dd2efb7fe5484606dfb
Ryan Palazón <ryan.palazon@enti.cat>
2021-03-03 18:29:46 +0100

    update particle system

--- code/src/physics.cpp
+++ code/src/physics.cpp
@@ -1,11 +1,19 @@
 #include <imgui\imgui.h>
 #include <imgui\imgui_impl_sdl_gl3.h>
+#include <glm/glm.hpp>
 
 //Exemple
 extern void Exemple_GUI();
 extern void Exemple_PhysicsInit();
 extern void Exemple_PhysicsUpdate(float dt);
 extern void Exemple_PhysicsCleanup();
+
+namespace LilSpheres {
+	extern int firstParticleIdx, particleCount;
+	extern const int maxParticles;
+	extern void updateParticles(int startIdx, int count, float* array_data);
+}
+extern bool renderParticles;
 
 bool show_test_window = false;
 void GUI() {
@@ -20,14 +28,57 @@ void GUI() {
 	ImGui::End();
 }
 
+
+//class Particle
+//{
+//public:
+//	Particle() : position(glm::vec3(0.f, 0.f, 0.f)) {};
+//	Particle(glm::vec3 _position) : position(_position) {};
+//	glm::vec3 position;
+//};
+
+class ParticleSystem
+{
+public:
+	ParticleSystem(int numParticles /*, (posicions opcionals)*/) {};
+
+	void updateParticle(int idx, glm::vec3 newPos)
+	{
+
+	}
+
+	void updateLilSpheres()
+	{
+
+	}
+
+private:
+
+};
+
+ParticleSystem ps;
+
 void PhysicsInit() {
-	Exemple_PhysicsInit();
+	//Exemple_PhysicsInit();
+	renderParticles = true;
+	ps = ParticleSystem(1);
+
+	ps.updateParticle(0, glm::vec3(4.f, 3.f, 2.f));
+
+	LilSpheres::firstParticleIdx = 0;
+	LilSpheres::particleCount = 1;
 }
 
 void PhysicsUpdate(float dt) {
-	Exemple_PhysicsUpdate(dt);
+	//Exemple_PhysicsUpdate(dt);
+
+	//int startParticle = 0;
+	//int numParticles = 1;
+	//float* particlesPositionVector = &(p1.position.x);
+
+	//LilSpheres::updateParticles(startParticle, numParticles, particlesPositionVector);
 }
 
 void PhysicsCleanup() {
-	Exemple_PhysicsCleanup();
+	//Exemple_PhysicsCleanup();
 }
